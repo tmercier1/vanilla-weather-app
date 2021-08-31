fd47aac4d355f1acbff2c53e34ac6f9e0ebc36b2
tmercier1 <86819629+tmercier1@users.noreply.github.com>
2021-08-31 04:03:58 -0700

    Cleaned up code and added unit conversion

--- src/app.js
+++ src/app.js
@@ -17,7 +17,6 @@ function formatDate(now) {
       "Thursday",
       "Friday",
       "Saturday",
-      "Sunday"
     ];
     let day = days[now.getDay()];
   
@@ -26,23 +25,24 @@ function formatDate(now) {
     return h2.innerHTML;
   }
   console.log(formatDate(new Date()));
-  
-  function search(event) {
-    event.preventDefault();
-    let cityElement = document.querySelector("#city");
-    let cityInput = document.querySelector("#city-input");
-    cityElement.innerHTML = cityInput.value;
-  
+
+  function search(city) {
     let apiKey = "d617c3d443d0373f72548b3cda3fe85a";
     let units = "imperial";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${apiKey}&units=metric${units}`;
-  
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+
     axios.get(apiUrl).then(showWeather);
   }
-  let searchForm = document.querySelector("#search-form");
-  searchForm.addEventListener("submit", search);
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    let cityElement = document.querySelector("#city-input");
+    search(cityElement.value);
+  
+  }
   
   function showWeather(response) {
+
     let city = document.querySelector("#city");
     city.innerHTML = response.data.name;
   
@@ -57,12 +57,11 @@ function formatDate(now) {
   
     let humidity = document.querySelector("#humidity");
     humidity.innerHTML = Math.round(response.data.main.humidity);
-  
-    let apiKey = "d617c3d443d0373f72548b3cda3fe85a";
-    let units = "imperial";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-  
-    axios.get(apiUrl).then(showWeather);
+
+    let iconElement = document.querySelector("#icon");
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+
+    fahrenheitTemperature = response.data.main.temp;
   }
   
   function showPosition(position) {
@@ -80,7 +79,36 @@ function formatDate(now) {
     event.preventDefault();
     navigator.geolocation.getCurrentPosition(showPosition);
   }
-  
+
+  function showCelsiusTemperature(event) {
+    event.preventDefault();
+    let temperatureElement = document.querySelector("#temperature");
+    fahrenheitLink.classList.remove("active");
+    celsiusLink.classList.add("active");
+    let celsiusTemperature = (fahrenheitTemperature - 32) * 5/9;
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  }
+
+  function showFahrenheitTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  }
+
+  let fahrenheitTemperature = null;
+
   let currentButton = document.querySelector(".current-button");
   currentButton.addEventListener("click", findCity);
-  
+
+  let celsiusLink = document.querySelector("#celsius-link");
+  celsiusLink.addEventListener("click", showCelsiusTemperature);
+
+  let fahrenheitLink = document.querySelector("#fahrenheit-link");
+  fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
+
+  let searchForm = document.querySelector("#search-form");
+  searchForm.addEventListener("submit", handleSubmit);
+
+  search("Los Angeles");
